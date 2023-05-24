07529fcb6389372386ee95051cfbd56e440a1479
florine89 <114070434+florine89@users.noreply.github.com>
2023-05-24 16:26:11 +0200

    structureok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="relative flex w-full flex-wrap items-center justify-between uppercase text-opacity-100
+         bg-green-200 text-neutral-500 shadow-lg hover:text-neutral-700
+          focus:text-neutral-700 dark:bg-neutral-600 p-2">
+<a href='/plant' className='transform motion-safe:hover:scale-110'>Les plantes</a>
+<a href='/Give' className='transform motion-safe:hover:scale-110'>Donner</a>
+<a href='/Find' className='transform motion-safe:hover:scale-110'>Rechercher</a>
+<a href='/Around' className='transform motion-safe:hover:scale-110'>Autour de moi</a>
+<a href='/Profil' className='transform motion-safe:hover:scale-110'>Profil</a>
+
+<div className='flex flex-col pr-4 text-sm w-40'>
+  <a href='/Register' className='border-1 m-1 border-emerald-700 hover:bg-green-400
+  rounded-md p-1 flex justify-center shadow-lg'>Sinscrire</a>
+  <a href='/Login' className='border-1 m-1 border-emerald-700  hover:bg-green-400
+  rounded-md p-1 flex justify-center shadow-lg'>Se connecter</a>
+</div>
+        </nav>
+
+        <div>{children}</div>
+
+
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-  <h1>hello</h1>
+  <h1>The main page (page.tsx)</h1>
   )
 }
