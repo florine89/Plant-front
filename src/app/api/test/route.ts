e447e83a0a009a73d1c82f3d1df8cb706b693153
florine89 <114070434+florine89@users.noreply.github.com>
2023-05-25 23:26:04 +0200

    newPagePlant

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,3 +1,5 @@
+// j'ai testé mon api avec cette route en json sur le navigateur
+
 import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://trefle.io/api/v1/plants?token=vnrixh9S9eLoI82jjRKpjR5GB14W31ySQcOrsMD1JGg"
---
+++ src/app/types/index.ts
@@ -0,0 +1,9 @@
+// içi je créée mon fivhier qui va gérer les types à l'aide de typescript
+// ça va me servir à faire appel à mes paramètre de mes datas 
+// et je vais m'en servir dans mes pages
+
+export type optionType = {
+    slug: string
+    image_url: string
+    plant: string
+}
