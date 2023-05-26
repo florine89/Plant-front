4bf43976080b625987480df7dd7e98e41d0c91a6
florine89 <114070434+florine89@users.noreply.github.com>
2023-05-26 13:50:25 +0200

    updateMainPage

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export function Nav(){
     return (
         <nav className="relative flex w-full flex-wrap items-center justify-between uppercase text-opacity-100
-        bg-green-200 text-neutral-500 shadow-lg hover:text-neutral-700
+        bg-green-50 text-neutral-500 shadow-lg hover:text-neutral-700
          focus:text-neutral-700 dark:bg-neutral-600 p-2">
          <Link href='/plant' className='transform transition duration-500 hover:scale-110 '>Les plantes</Link>
          <Link href='/Give' className='transform transition duration-500 hover:scale-110'>Donner</Link>
@@ -12,10 +12,10 @@ export function Nav(){
          <Link href='/Profil' className='transform transition duration-500 hover:scale-110'>Profil</Link>
 
        <div className='flex flex-col pr-4 text-sm w-40'>
-           <a href='/Register' className='border-1 m-1 border-emerald-700 hover:bg-green-400
+           <a href='/Register' className='border-1 m-1 border-emerald-700 hover:bg-dark-green
  transform transition duration-500 hover:scale-110
  rounded-md p-1 flex justify-center shadow-lg'>Sinscrire</a>
- <a href='/Login' className='border-1 m-1 border-emerald-700  hover:bg-green-400
+ <a href='/Login' className='border-1 m-1 border-emerald-700  hover:bg-dark-green
  transform transition duration-500 hover:scale-110
  rounded-md p-1 flex justify-center shadow-lg'>Se connecter</a>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'custom-green':'#83B192',
+        'dark-green':'#5a9672'
+        }
     },
   },
   plugins: [],
